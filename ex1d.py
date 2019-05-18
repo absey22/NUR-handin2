583ede4692e4a3e3c498f7c1119d831e5ae4f91f
Seymour Aaron <seymour@student34.strw.leidenuniv.nl>
2019-05-19 00:06:07 +0200

    initial push: ex1 not yet working

---
+++ ex1d.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# ==========================  1(d)   ==========================
+from ex1functions import kuiperstest,BoxMuller, GaussianCDF
+from astropy.stats import kuiper
+from scipy.stats import kstest
+
+#Kuiper V = D+ + D- is a statistic that is invariant under all shifts
+# and parametrizations on a circle created by wrapping around the x-axis.
+
+#from kuiper source code:
+"""
+    Stephens 1970 claims this is more effective than the KS at detecting
+    changes in the variance of a distribution; the KS is (he claims) more
+    sensitive at detecting changes in the mean.
+    If cdf was obtained from data by fitting, then fpp is not correct and
+    it will be necessary to do Monte Carlo simulations to interpret D.
+    D should normally be independent of the shape of CDF.
+"""
+
+#See ex1c.py for comments explaining these steps here:
+
+N=int(1e5)
+randomdata=BoxMuller(N,mean=0,stdv=1.0)
+
+dex=0.1 
+start,stop=1,5 
+cnt=np.ones(int((stop-start)/dex)+1)
+mykuiper=np.zeros((len(cnt),3))
+scipykuiper=np.zeros((len(cnt),2))
+
+for i in range(len(cnt)):
+    cnt[i]+=round(i*dex,2)
+    dataslice=randomdata[:int(10**cnt[i])]
+    mykuiper[i]=kuiperstest(dataslice) # get D+,D-,pval from my Kuiper test
+    scipykuiper[i,0]=kstest(dataslice,'norm',alternative='greater')[0] # get astropy D+
+    scipykuiper[i,1]=kstest(dataslice,'norm',alternative='less')[0] # get scipy D-
+   
+
+#Make a plot comparing the D+and D- statistics 
+plt.figure()
+plt.subplot(2,1,1)
+plt.title("Kuiper Test (D+ & D-)")
+plt.plot(cnt,mykuiper[:,0],label="My Kuiper D+ statistic")
+plt.plot(cnt,scipykuiper[:,0],label="Scipy Kuiper D+ statistic")
+plt.ylabel("D+ statistic")
+#plt.yscale("log")
+#plt.ylim(top=(10**0.2))
+plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(cnt,mykuiper[:,1],label="My Kuiper D- statistic")
+plt.plot(cnt,scipykuiper[:,1],label="Scipy D- statistic")
+plt.xlabel("$log_{10}(N_{points})$")
+plt.ylabel("D- statistic")
+#plt.yscale("log")
+#plt.ylim(top=(10**0.2))
+plt.legend()
+
+
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+plt.savefig("./plots/1d_kuipertest_lohi.png")
+plt.clf()
+
+
+
+#make the same plot as 1(c):
+
+dex=0.1 
+start,stop=1,5 
+cnt=np.ones(int((stop-start)/dex)+1)
+mykuiper=np.zeros((len(cnt),2))
+astropykuiper=mykuiper.copy()
+from math import erf
+from astropy.modeling.functional_models import Gaussian1D 
+for i in range(len(cnt)):
+    cnt[i]+=round(i*dex,2)
+    print(i)
+    dataslice=randomdata[:int(10**cnt[i])]
+    kuipertemp=kuiperstest(dataslice) # get D+,D-,pval from my Kuiper test
+    mykuiper[i]=kuipertemp[0]+kuipertemp[1],kuipertemp[2] # store those
+    print(mykuiper[i])
+    astropykuiper[i]=kuiper(dataslice,lambda x:0.5*( 1.-erf( -(x-0.0)/(1.0*2**0.5) ) ) ) # get astropy D,pval
+    print("kuiper:",astropykuiper[i])
+
+
+plt.subplot(2,1,1)
+plt.title("Kuiper Test: Box-Muller Normal rvs")
+plt.plot(cnt,mykuiper[:,1],label="My Kuiper p-value")
+plt.plot(cnt,scipykuiper[:,1],label="AstroPy p-value")
+plt.xlabel("$log_{10}(N_{points})$")
+plt.ylabel("p-value")
+plt.hlines(0.05,0.8,5.2,linewidth=0.8,linestyles=':')
+plt.text(4.35,10**-1.6,'2-$\sigma$ rejection',color='r')
+plt.hlines(0.003,0.8,5.2, linewidth=0.8,linestyles='--')
+plt.text(4.35,10**-2.82,'3-$\sigma$ rejection',color='r')
+plt.yscale("log")
+plt.ylim((10**-3,10**0.2))
+plt.legend(loc=1)
+
+plt.subplot(2,1,2)
+plt.plot(cnt,mykuiper[:,0],label="My Kuiper V statistic")
+plt.plot(cnt,scipykuiper[:,0],label="AstroPy V statistic")
+plt.xlabel("$log_{10}(N_{points})$")
+plt.ylabel("V statistic")
+plt.yscale("log")
+plt.ylim(top=(10**0.2))
+plt.legend()
+
+
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+plt.savefig("./plots/1d_kuipertest.png")
+plt.clf()
+
---
+++ ex1e.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# ==========================  1(e)   ==========================
+from ex1functions import kuiperstest,GaussianPDF
+
+#Kuiper test will be robust against cyclic data with a phase in the x-direction
+
+dataset=np.genfromtxt("randomnumbers.txt")
+
+
+#for i in range(dataset.shape[1]):
+#    plt.subplot(2,5,i+1)
+#    plt.hist(dataset[:,i],bins=20,edgecolor='black', linewidth=1.5,density=True)
+#    plt.plot(np.linspace(-5,5,55),GaussianPDF(np.linspace(-5,5,55)),linewidth=2,linestyle="#--",color='k')
+#    plt.xlim((-5,5))
+#    plt.ylim((0,0.45))
+#plt.title("Normalized Datasets ('randomnumbers.txt')")
+#plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+#plt.savefig("./plots/1e_datasets.png")
+#plt.clf()
+
+
+for j in range(dataset.shape[1]):
+    dex=0.1 
+    start,stop=1,5 
+    cnt=np.ones(int((stop-start)/dex)+1)
+    mykuiper=np.zeros((len(cnt),3))
+
+    data=dataset[:,j] # FOR EACH COLUMN in dataset:: take one of the data sets
+
+    for i in range(len(cnt)): #slice it incrementally
+        cnt[i]+=round(i*dex,2)
+        dataslice=data[:int(10**cnt[i])]
+        mykuiper[i]=kuiperstest(dataslice) # get D+,D-,pval from my Kuiper test
+
+    
+    plt.subplot(3,1,1)
+    plt.title("Kuiper Test: 'randomnumbers.txt' Dataset #"+str(j+1))
+    plt.hist(data,bins=20,edgecolor='black', linewidth=1.5,density=True)
+    plt.plot(np.linspace(-5,5,55),GaussianPDF(np.linspace(-5,5,55)),linewidth=2,linestyle=":",color='k')
+    plt.xlabel("True Values")
+    plt.ylabel("normlzd. rv freq.")
+    plt.xlim((-5,5))
+    plt.ylim((0,0.45))
+
+    plt.subplot(3,1,2)
+    plt.plot(cnt,mykuiper[:,2],'r',label="My Kuiper p-value")
+    plt.xlabel("$log_{10}(N_{points})$")
+    plt.ylabel("p-value")
+    plt.hlines(0.05,0.8,5.2,linewidth=0.8,linestyles=':')
+    plt.text(4.35,10**-1.8,'2-$\sigma$ rejection',fontsize=10,color='r')
+    plt.hlines(0.003,0.8,5.2, linewidth=0.8,linestyles='--')
+    plt.text(4.35,10**-3.2,'3-$\sigma$ rejection',fontsize=10,color='r')
+    plt.hlines(0.0001,0.8,5.2, linewidth=0.8,linestyles='-')
+    plt.text(4.35,10**-4.6,'4-$\sigma$ rejection',color='r')
+    plt.yscale("log")
+    plt.ylim((10**-5.5,10**0.5))
+    plt.legend(loc=3)
+
+    plt.subplot(3,1,3)
+    plt.plot(cnt,np.add(mykuiper[:,0],mykuiper[:,1]),label="My Kuiper V statistic")
+    plt.xlabel("$log_{10}(N_{points})$")
+    plt.ylabel("V statistic")
+    plt.yscale("log")
+    plt.ylim((10**-2.5,10**0.5))
+    plt.legend(loc=3)
+
+
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    plt.savefig("./plots/1e_kuipertest_data"+str(j+1)+".png")
+    plt.clf()
